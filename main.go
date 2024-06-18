8f42a9a328cfb850c30eaaf3e810ddc38e7da029
Felipe <felpandrade01@gmail.com>
2024-06-18 19:24:50 -0300

    adicionando o código

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instanciano o coletor, a partir do módulo Colly
+	c := colly.NewCollector()
+
+	// Lista pra receber os dados
+	var filmes []string
+
+	// Encontrar o que queremos extrair
+	c.OnHTML(".ipc-metadata-list-summary-item__c", func(e *colly.HTMLElement) {
+		filme := e.ChildText(".ipc-title__text")
+		filmes = append(filmes, filme)
+	})
+
+	// Aqui vamos inserir o link da página que queremos extrair os dados
+	c.Visit("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+
+	// Imprime os nomes dos times coletados
+	for _, filme := range filmes {
+		fmt.Println("Filme:", filme)
+	}
+}
